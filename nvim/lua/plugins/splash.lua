a67e093e8168830ae8ece4673391ff296ffd7b92
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-06-23 23:07:33 +0100

    Create splash.lua

---
+++ nvim/lua/plugins/splash.lua
@@ -0,0 +1,33 @@
+-- splash.lua
+
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      "                                                         ",
+      "  ███████╗████████╗███████╗██╗   ██╗██╗███████╗██████╗   ",
+      "  ██╔════╝╚══██╔══╝██╔════╝██║   ██║██║██╔════╝██╔══██╗  ",
+      "  ███████╗   ██║   █████╗  ██║   ██║██║█████╗  ██████╔╝  ",
+      "  ╚════██║   ██║   ██╔══╝  ╚██╗ ██╔╝██║██╔══╝  ██╔═══╝   ",
+      "  ███████║   ██║   ███████╗ ╚████╔╝ ██║███████╗██║       ",
+      "  ╚══════╝   ╚═╝   ╚══════╝  ╚═══╝  ╚═╝╚══════╝╚═╝       ",
+      "                                                         ",
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "  Find File", ":FzfLua files<CR>"),
+      dashboard.button("r", "  Recent Files", ":FzfLua oldfiles<CR>"),
+      dashboard.button("g", "  Live Grep", ":FzfLua live_grep<CR>"),
+      dashboard.button("s", "  Buffers", ":FzfLua buffers<CR>"),
+      dashboard.button("b", "  Git Branch", ":FzfLua git_branches<CR>"),
+      dashboard.button("c", "  Config", ":FzfLua files cwd=~/.config/nvim<CR>"),
+      dashboard.button("l", "  Lazy", ":Lazy<CR>"),
+    }
+    
+    alpha.setup(dashboard.config)
+  end,
+}
