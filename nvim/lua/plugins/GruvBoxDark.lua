404a5438d27f3d898021f71ae409806ea25225cd
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-07-31 20:26:19 +0100

    Create GruvBoxDark.lua

---
+++ nvim/lua/plugins/GruvBoxDark.lua
@@ -0,0 +1,26 @@
+-- themes.lua
+
+return {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        require("gruvbox").setup({
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+            strings = true,
+            emphasis = true,
+            comments = true,
+            operators = false,
+            folds = true,
+        },
+        strikethrough = true,
+        contrast = "hard",
+        dim_inactive = false,
+        transparent_mode = true,
+        })
+        vim.cmd("colorscheme gruvbox")
+    end,
+}
