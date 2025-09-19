a6f39c49a1d6759931712dfb47372f78ad8559db
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-09-19 17:54:57 +0100

    Added token concealer

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,6 +98,21 @@ vim.api.nvim_create_autocmd(
   }
 )
 
+ -- Conceal secrets in environment and script files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.*config", "*.*rc", "*.env*", "*.sh" },
+  group = vim.api.nvim_create_augroup("conceal_secrets", { clear = true }),
+  callback = function()
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.concealcursor = "nc"
+    vim.api.nvim_set_hl(0, "Conceal", { link = "String" })
+    local keywords = "API|AUTH|ENDPOINT|ENTERPRISE|KEY|SECRET|TOKEN|URL"
+    vim.fn.matchadd("Conceal", [[\v\w*%(]] .. keywords .. [[)\w*\="\zs[^"]+\ze"]], 10, -1, { conceal = "A TOKEN WAS HERE" })
+    vim.fn.matchadd("Conceal", [[\v\w*%(]] .. keywords .. [[)\w*\='\zs[^']+\ze']], 10, -1, { conceal = "A TOKEN WAS HERE" })
+    vim.fn.matchadd("Conceal", [[\v\w*%(]] .. keywords .. [[)\w*\=\zs[^"'\s]+]], 10, -1, { conceal = "A TOKEN WAS HERE" })
+  end,
+})
+
 -- Lazy Plugin Manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
