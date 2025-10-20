49b4118da31b76509ef7a5b27b47e7ace2b7c8f3
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-10-20 22:50:52 +0100

    Update FZF_DEFAULT_OPTS color settings

--- bin/opts.sh
+++ bin/opts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export FZF_DEFAULT_OPTS="
---color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9
---color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
---color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
---height=100% --reverse --border=rounded
-"
+--color=fg:#ddc7a1,bg:#292828,hl:#d8a657 
+--color=fg+:#ddc7a1,bg+:#4a4341,hl+:#d8a657 
+--color=info:#d8a657,prompt:#a9b665,pointer:#ea6962 
+--height=100% --reverse --border=rounded"
+
 
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=yellow,bold,underline'
 
