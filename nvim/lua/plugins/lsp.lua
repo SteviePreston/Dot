daa9e519bb3a1a472e7c11acde5296bdad32fa0c
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-10-20 22:55:15 +0100

    Replace cmp_nvim_lsp with blink.cmp for LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,52 +5,55 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
+		"saghen/blink.cmp",
 	},
-
 	config = function()
 		local lspconfig = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-    vim.diagnostic.config({
-      virtual_lines = false,
-      virtual_text = false,
-      underline = true,
-      update_in_insert = false,
-      severity_sort = true,
-      float = {
-        border = "rounded",
-        source = true,
-        focusable = false,
-        style = "minimal",
-        max_width = 80,
-        wrap = true,
-      },
-      signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
-      },
-    })
-
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous LSP diagnostic" })
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next LSP diagnostic" })
-
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		
+		vim.diagnostic.config({
+			virtual_lines = false,
+			virtual_text = false,
+			underline = true,
+			update_in_insert = false,
+			severity_sort = true,
+			float = {
+				border = "rounded",
+				source = true,
+				focusable = false,
+				style = "minimal",
+				max_width = 80,
+				wrap = true,
+			},
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "󰅚 ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.INFO] = "󰋽 ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+					[vim.diagnostic.severity.WARN] = "WarningMsg",
+				},
+			},
+		})
+		
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "LSP: Go to previous LSP diagnostic" })
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "LSP: Go to next LSP diagnostic" })
+		
 		local on_attach = function(client, bufnr)
 			if client.server_capabilities.documentFormattingProvider then
 				vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
 			end
 		end
-
-		local common_setup = { capabilities = capabilities, on_attach = on_attach, autostart = true }
-
+		
+		local common_setup = { 
+			capabilities = capabilities, 
+			on_attach = on_attach, 
+			autostart = true 
+		}
+		
 		require("mason").setup()
 		require("mason-lspconfig").setup({
 			ensure_installed = {
@@ -60,13 +63,17 @@ return {
 				"terraformls",
 			},
 			automatic_installation = true,
+			handlers = {
+				function(server_name)
+					lspconfig[server_name].setup(common_setup)
+				end,
+			},
 		})
-
+		
 		vim.api.nvim_create_autocmd("CursorHold", {
 			callback = function()
 				vim.diagnostic.open_float(nil, { focusable = false })
 			end,
 		})
-
 	end,
 }
