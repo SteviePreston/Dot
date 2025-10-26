c4654f741f709f8b33b0f67870e61c59f4273f43
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-10-26 21:31:23 +0000

    Add color schemes for terminal options

--- bin/opts.sh
+++ bin/opts.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-export FZF_DEFAULT_OPTS="
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=yellow,bold,underline"
+export FZF_DEFAULT_OPTS="$GRUVBOX_DARK"
+
+GRUVBOX_DARK="
+--color=fg:#ddc7a1,bg:#1d2021,hl:#d8a657
+--color=fg+:#ddc7a1,bg+:#3c3836,hl+:#fabd2f
+--color=info:#83a598,prompt:#d3869b,pointer:#fe8019,marker:#b8bb26,spinner:#fabd2f,header:#665c54
+--height=100% --layout=reverse --border=rounded"
+
+GRUVBOX_MATERIAL_DARK="
 --color=fg:#ddc7a1,bg:#292828,hl:#d8a657 
 --color=fg+:#ddc7a1,bg+:#4a4341,hl+:#d8a657 
 --color=info:#d8a657,prompt:#a9b665,pointer:#ea6962 
 --height=100% --reverse --border=rounded"
 
 
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=yellow,bold,underline'
+CATPPUCCIN_MOCHA="
+--color=fg:#cdd6f4,bg:#1e1e2e,hl:#f9e2af
+--color=fg+:#cdd6f4,bg+:#313244,hl+:#f9e2af
+--color=info:#89b4fa,prompt:#f38ba8,pointer:#fab387,marker:#a6e3a1,spinner:#f9e2af,header:#585b70
+--height=100% --layout=reverse --border=rounded"
 
+DRACULA="
+--color=fg:#f8f8f2,bg:#282a36,hl:#ff79c6
+--color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
+--color=info:#8be9fd,prompt:#ffb86c,pointer:#ff5555,marker:#50fa7b,spinner:#f1fa8c,header:#6272a4
+--height=100% --layout=reverse --border=rounded"
