a78735f6aace402f0f94092ce09f58611ffeed93
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-10-26 21:27:56 +0000

    Refactor GruvBoxDark configuration and add lualine

--- nvim/lua/plugins/GruvBoxDark.lua
+++ nvim/lua/plugins/GruvBoxDark.lua
@@ -1,27 +1,45 @@
 -- themes.lua
 
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    lazy = true,
-    config = function()
-        require("gruvbox").setup({
-        terminal_colors = true,
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-            strings = true,
-            emphasis = true,
-            comments = true,
-            operators = false,
-            folds = true,
-        },
-        strikethrough = true,
-        contrast = "hard",
-        dim_inactive = false,
-        transparent_mode = true,
-        })
-        vim.cmd("colorscheme gruvbox")
-    end,
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true,
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = false,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true,
+                contrast = "hard",
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+            vim.cmd.colorscheme('gruvbox')
+        end,
+    },
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				theme = vim.g.colors_name,
+				refresh = {
+					statusline = 1000,
+				},
+			},
+		},
+	},
 }
