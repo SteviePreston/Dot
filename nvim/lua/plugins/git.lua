6b20ca7d0c3c3c166926463f1afa0ca041670ff7
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-11-03 21:44:09 +0000

    Update Gitsigns configuration and keybindings

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -14,7 +14,7 @@ return {
 			},
 			signcolumn = true,
 			numhl = false, 
-      linehl = false, 
+            linehl = false, 
 			watch_gitdir = {
 				interval = 1000,
 				follow_files = true,
@@ -25,15 +25,23 @@ return {
 			},
 			current_line_blame_formatter = "<author> - <summary> (<author_time:%d-%m-%Y>)",
 			max_file_length = 5000,
+            preview_config = {
+				border = "rounded",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
 		})
+		
 		-- Keybindings for Gitsigns
 		vim.keymap.set("n", "]g", ":Gitsigns next_hunk<CR>", { desc = "GIT: Next Git Hunk" })
 		vim.keymap.set("n", "[g", ":Gitsigns prev_hunk<CR>", { desc = "GIT: Previous Git Hunk" })
 
 		vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "GIT: Reset Git Hunk" })
 		vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "GIT: Stage Git Hunk" })
+		vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_buffer<CR>", { desc = "GIT: Reset Buffer" })
 
-		vim.keymap.set("n", "<leader>grb", ":Gitsigns reset_buffer<CR>", { desc = "GIT: Reset Buffer" })
 		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { desc = "GIT: Blame Current Line" })
 	end,
 }
